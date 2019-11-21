5922dfec5c27fcd6e61db817fc15f88bb2103677
stachelek11 <stachelek11@gmail.com>
2019-11-21 08:25:51 +0100

    update Cw04_stachelek

--- Wypozyczalnia/MojProjekt/books/models.py
+++ Wypozyczalnia/MojProjekt/books/models.py
@@ -6,6 +6,9 @@ class Klient(models.Model):
     Imię = models.CharField(max_length=45)
     Nazwisko = models.CharField(max_length=45)
     Numer_Prawa_Jazdy = models.CharField(max_length=13)
+    def __str__(self):
+        return '%s %s %s %s' %(self.PESEL, self.Imię, self.Nazwisko, self.Numer_Prawa_Jazdy)
+
 
 class Samochód(models.Model):
     VIN = models.CharField(primary_key=True, max_length=100)
@@ -13,12 +16,18 @@ class Samochód(models.Model):
     Model = models.CharField(max_length=45)
     Dostępność = models.IntegerField()
     CenaZaDobę = models.FloatField()
+    def __str__(self):
+        return '%s %s %s %s %s' % (self.VIN, self.Marka, self.Model, self.Dostępność, self.CenaZaDobę)
+
 
 class Pracownik(models.Model):
     idPracownik = models.IntegerField(primary_key=True)
     Imię = models.CharField(max_length=45)
     Nazwisko = models.CharField(max_length=45)
     Stanowisko = models.CharField(max_length=45)
+    def __str__(self):
+        return '%s %s %s %s' % (self.idPracownik, self.Imię, self.Nazwisko, self.Stanowisko)
+
 
 class Wypożyczanie(models.Model):
     idWypożyczanie = models.IntegerField(primary_key=True)
@@ -28,3 +37,6 @@ class Wypożyczanie(models.Model):
     Data_rozpoczęcia = models.DateField()
     Data_zakończenia = models.DateField()
     Cena_za_wypożyczenie = models.FloatField()
+    def __str__(self):
+        return '%s %s %s %s %s %s %s' % (self.idWypożyczanie, self.Klient_PESEL, self.Samochód_VIN, self.Pracownik_idPracownika, self.Data_rozpoczęcia,
+                                   self.Data_zakończenia, self.Cena_za_wypożyczenie)
